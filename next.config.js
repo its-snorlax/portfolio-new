2ebe6b019a668f919e5b5b37f414221d21e05575
Jain Prayas <p.jain3322@gmail.com>
2025-03-17 23:49:24 +0530

    Update Next.js config for GitHub Pages asset paths

--- next.config.js
+++ next.config.js
@@ -1,10 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  trailingSlash: true,
+  basePath: '/portfolio-new',
+  assetPrefix: '/portfolio-new/',
   images: {
     unoptimized: true,
   },
-  trailingSlash: true,
 };
 
 module.exports = nextConfig; 
