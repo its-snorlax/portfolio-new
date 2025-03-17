a8151ce53a5b2e5ba46cf1a36a174a13c2cf9995
Jain Prayas <p.jain3322@gmail.com>
2025-03-17 22:33:31 +0530

    Add GitHub Pages deployment configuration

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
     unoptimized: true,
   },
   basePath: process.env.NODE_ENV === 'production' ? '/my-portfolio' : '',
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-portfolio/' : '',
 };
 
 module.exports = nextConfig; 
