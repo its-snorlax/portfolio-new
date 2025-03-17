2d068632e62b2cbda82046a831b033e695285fa6
Jain Prayas <p.jain3322@gmail.com>
2025-03-17 23:58:07 +0530

    Add path utility for handling image paths in different environments

--- next.config.js
+++ next.config.js
@@ -1,12 +1,16 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production'
+const repoName = 'portfolio-new'
+
 const nextConfig = {
   output: 'export',
   trailingSlash: true,
-  basePath: '/portfolio-new',
-  assetPrefix: '/portfolio-new/',
+  basePath: isProduction ? `/${repoName}` : '',
+  assetPrefix: isProduction ? `/${repoName}` : '',
   images: {
     unoptimized: true,
+    path: isProduction ? `/${repoName}/_next/image` : '/_next/image'
   },
-};
+}
 
-module.exports = nextConfig; 
+module.exports = nextConfig 
